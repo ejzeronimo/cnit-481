f6425b9ad5b748b4d11f46224d4e904faa48e7ae
Ethan Zeronik <ejzeronik@gmail.com>
2023-04-18 16:10:14 -0400

    finished step one

--- Lab Nine/createSurvey.js
+++ Lab Nine/createSurvey.js
@@ -1,66 +1,95 @@
 const video_categories = [
     {
-      "id": "1",
-      "title": "Film & Animation"
+        "id": "1",
+        "title": "Film & Animation"
     },
     {
-      "id": "2",
-      "title": "Autos & Vehicles"
+        "id": "2",
+        "title": "Autos & Vehicles"
     },
     {
-      "id": "10",
-      "title": "Music"
+        "id": "10",
+        "title": "Music"
     },
     {
-      "id": "15",
-      "title": "Pets & Animals"
+        "id": "15",
+        "title": "Pets & Animals"
     },
     {
-      "id": "17",
-      "title": "Sports"
+        "id": "17",
+        "title": "Sports"
     },
     {
-      "id": "19",
-      "title": "Travel & Events"
+        "id": "19",
+        "title": "Travel & Events"
     },
     {
-      "id": "20",
-      "title": "Gaming"
+        "id": "20",
+        "title": "Gaming"
     },
     {
-      "id": "22",
-      "title": "People & Blogs"
+        "id": "22",
+        "title": "People & Blogs"
     },
     {
-      "id": "23",
-      "title": "Comedy"
+        "id": "23",
+        "title": "Comedy"
     },
     {
-      "id": "24",
-      "title": "Entertainment"
+        "id": "24",
+        "title": "Entertainment"
     },
     {
-      "id": "25",
-      "title": "News & Politics"
+        "id": "25",
+        "title": "News & Politics"
     },
     {
-      "id": "26",
-      "title": "Howto & Style"
+        "id": "26",
+        "title": "Howto & Style"
     },
     {
-      "id": "27",
-      "title": "Education"
+        "id": "27",
+        "title": "Education"
     },
     {
-      "id": "28",
-      "title": "Science & Technology"
+        "id": "28",
+        "title": "Science & Technology"
     },
     // {
     //   "id": "29",
     //   "title": "Nonprofits & Activism"
     // }
-  ]
+]
 
 const choices = ["like", "neutral", "dislike"];
 
-// STEP 1: Create surevy
+// NOTE: creating the survey
+const targetForm = document.getElementById("preference-survey");
+
+// iterate over the categories
+for (const category of video_categories) {
+    let catName = document.createElement("p");
+    catName.innerText = category.title;
+    catName.id = "video-id-" + category.id;
+
+    let catQuestion = document.createElement("div");
+    catQuestion.appendChild(catName);
+
+    // iterate over the possible choices
+    for (choice in choices) {
+        let button = document.createElement("input");
+        button.value = choices[choice];
+        button.id = ["preference", category.id, choice].join("-");
+        button.name = "preference-" + category.id;
+        button.type = "radio";
+
+        let label = document.createElement("label");
+        label.innerText = choices[choice];
+        label.for = ["preference", category.id, choice].join("-");
+
+        catQuestion.appendChild(button);
+        catQuestion.appendChild(label);
+    }
+
+    targetForm.appendChild(catQuestion);
+}
