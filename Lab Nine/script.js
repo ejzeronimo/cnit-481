46702e67477e25a5766112d3a2e51e821f9d0640
Ethan Zeronik <ejzeronik@gmail.com>
2023-04-18 16:10:14 -0400

    finished step one

--- Lab Nine/script.js
+++ Lab Nine/script.js
@@ -1,39 +1,39 @@
 const preferenceSurvey = preferenceSurveyDiv.parentElement;
 const videoDiv = document.getElementById("videos");
 const YOUTUBE_API_KEY = "";
-const threshold = window.innerHeight/2;
+const threshold = window.innerHeight / 2;
 
 // STEP 5: can you explain what the following code does? Please describe the user interaction it enables. A user interaction can be: "every time the user click on the button, the color of the header will get darker."
-preferenceSurvey.addEventListener("change", function(e){
+preferenceSurvey.addEventListener("change", function (e) {
     loadVideo();
 });
 
 // STEP 5: can you explain what the following code does? Please describe the user interaction it enables. A user interaction can be: "every time the user click on the button, the color of the header will get darker."
-preferenceSurvey.addEventListener("submit", function(e){
+preferenceSurvey.addEventListener("submit", function (e) {
     e.preventDefault();
     videoDiv.scrollIntoView();
 })
 
 // STEP 5: can you explain what the following code does? Please describe the user interaction it enables. A user interaction can be: "every time the user click on the button, the color of the header will get darker." 
-window.addEventListener("scroll", function(){
-    for(myframe of document.querySelectorAll("iframe")){
+window.addEventListener("scroll", function () {
+    for (myframe of document.querySelectorAll("iframe")) {
         let myOffset = window.scrollY - myframe.offsetTop;
-        if(myOffset>(0-threshold) && myOffset < threshold){
+        if (myOffset > (0 - threshold) && myOffset < threshold) {
             myframe.contentWindow.postMessage(JSON.stringify({ event: 'command', func: 'playVideo' }), '*');
-        }else{
+        } else {
             myframe.contentWindow.postMessage(JSON.stringify({ event: 'command', func: 'stopVideo' }), '*');
         }
     }
 })
 
-
-function getQueryStr(){
+// NOTE: create a query string
+function getQueryStr() {
     // STEP 2
 }
 
-async function loadVideo(){
+async function loadVideo() {
     const query = getQueryStr();
-    
+
     const url = `https://www.googleapis.com/youtube/v3/search?key=${YOUTUBE_API_KEY}&type=video&part=snippet&maxResults=5&q=${query}&videoCategoryId=42`;
 
     // STEP 3 and STEP 4
